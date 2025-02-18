9b98a5e0c9527116645feda23b060a64ae6b6218
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2025-02-18 11:40:00 -0300

    Criando um form para adicação de novas anotações.

--- Aula 1-4/learning_log/learning_logs/views.py
+++ Aula 1-4/learning_log/learning_logs/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -39,4 +39,24 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Acresenta uma nova entrada para um assunto escolhido."""
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != 'POST':
+        # Nenhum dado enviado; cria um formulário em branco
+        form = EntryForm()
+    else:
+        # Dados de POST enviados; processa os dados
+        form = EntryForm(data=request.POST) 
+        if form.is_valid():
+            new_entry = form.save(commit=False) # Cria um objeto mas não salva no BD.
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+        
+    context = {'topic':topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
     
