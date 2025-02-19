8f508ad81c91610aca906a211890fd1e469a9f4b
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2025-02-19 19:37:43 -0300

    Criando a função de editar anotaões.

--- Aula 1-4/learning_log/learning_logs/views.py
+++ Aula 1-4/learning_log/learning_logs/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -43,7 +43,7 @@ def new_topic(request):
 
 def new_entry(request, topic_id):
     """Acresenta uma nova entrada para um assunto escolhido."""
-    topic = Topic.objects.get(id = topic_id)
+    topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
         # Nenhum dado enviado; cria um formulário em branco
@@ -57,6 +57,26 @@ def new_entry(request, topic_id):
             new_entry.save()
             return HttpResponseRedirect(reverse('topic', args=[topic_id]))
         
-    context = {'topic':topic, 'form':form}
+    context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edita uma entrada existente."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Requisição inicial; preenche previamente o formulário com a entrada atual.
+        form = EntryForm(instance=entry)
+    else:
+        # Dados de POST submetidos; processa os dados.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+        
+    context = {'entry': entry, 'topic': topic, 'form': form} 
+    return render(request, 'learning_logs/edit_entry.html', context)
+
     
